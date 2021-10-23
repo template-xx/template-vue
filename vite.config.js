453573039df33541672c219ba2dca0e2dd7c9a0e
codesktop <codesktop@foxmail.com>
2021-10-23 20:18:39 +0800

    init commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite'
 import vuePlugin from '@vitejs/plugin-vue'
 
-export default {
+export default defineConfig({
   plugins: [
     vuePlugin()
   ],
@@ -8,7 +9,6 @@ export default {
     manifest: true
   },
   optimizeDeps: {
-    include: [
-    ]
+    include: []
   }
-}
+})
