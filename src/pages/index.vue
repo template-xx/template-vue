d85098dc236501cc82d86bf659fe5d453cfb9144
tossode <tossdev@189.cn>
2022-04-05 16:52:16 +0800

    :lock: |> tossd

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Pokemon :data="items" />
+  </div>
+</template>
+
+<script setup>
+  import Pokemon from '../components/pokemon/index.vue'
+  import axios from '../plugins/axios'
+  import { useStore } from 'vuex'
+  import { computed } from 'vue'
+
+  const store = useStore()
+  const items = computed(() => store.state.pokemon.data)
+
+  axios.get('/pokemon').then(({ data }) => {
+    store.commit('pokemon/getData', data)
+  })
+</script>
