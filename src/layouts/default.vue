da87b7b35894b540f7ddde6b91f40cecaa2d948f
tossode <tossdev@189.cn>
2022-07-15 23:27:15 +0800

    tosslate created a commit :sparkles:

---
+++ src/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen">
+    <default-header></default-header>
+    <main class="max-w-4xl mx-auto">
+      <slot></slot>
+    </main>
+    <default-footer></default-footer>
+  </div>
+</template>
+
+<script setup>
+import DefaultHeader from '../components/header.vue'
+import DefaultFooter from '../components/footer.vue'
+</script>
