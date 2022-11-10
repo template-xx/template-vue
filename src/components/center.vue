ec3f8f476595ee5ed023821e35285f34e989cb74
tossode <tossdev@189.cn>
2022-11-10 21:17:56 +0800

    reset repository

--- src/components/center.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex items-center justify-center" :style="{ height }">
-    <slot></slot>
-  </div>
-</template>
-
-<script setup lang="ts">
-defineProps<{ height?: string }>()
-</script>
--- src/helpers/head.ts
+++
@@ -1,8 +0,0 @@
-class Head {
-  title(value: string) {
-    document.title = value
-    return this
-  }
-}
-
-export default new Head()
