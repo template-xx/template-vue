7b0691a37e198b937e146b69ce9db8d8d892d1f9
tossode <tossdev@189.cn>
2022-11-24 19:39:52 +0800

    tosslate created a commit :sparkles:

--- src/components/pokemon/card.vue
+++ src/components/pokemon/card.vue
@@ -4,16 +4,21 @@
       class="overflow-hidden rounded shadow-lg mb-2"
       :style="{ backgroundColor: data.color }"
     >
-      <div class="rounded shadow-inner p-4">
-        <img
-          :class="[
-            'duration-300 ease-out object-cover aspect-square group-hover:scale-110',
-            isLoading ? '-translate-y-6 scale-90' : 'translate-y-0 scale-100'
-          ]"
-          :alt="data.name"
-          :src="data.imageUrl"
-          @load="setLoading(false)"
-        />
+      <div class="relative rounded shadow-inner p-4">
+        <img class="invisible w-full" src="/placeholder.png" />
+        <div
+          class="absolute inset-4 duration-150 ease-out group-hover:scale-110"
+        >
+          <img
+            :class="[
+              'duration-300 ease-out object-cover aspect-square',
+              isLoading ? '-translate-y-6 scale-90' : 'translate-y-0 scale-100'
+            ]"
+            :alt="data.name"
+            :src="data.imageUrl"
+            @load="setLoading(false)"
+          />
+        </div>
       </div>
     </div>
     <h3 class="text-base text-neutral-700">{{ data.name }}</h3>
