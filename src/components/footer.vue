38cf999a1f5fc607bbc73ac0a800697f6f32924e
tossode <tossdev@189.cn>
2022-11-24 20:45:09 +0800

    reset repository

--- src/components/footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer>
-    <div class="max-w-6xl mx-auto"></div>
-  </footer>
-</template>
-
-<script setup lang="ts">
-defineProps<{ copyright?: string }>()
-</script>
--- src/layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="min-h-screen">
-    <slot></slot>
-  </div>
-</template>
